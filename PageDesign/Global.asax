56799759aa93fc330df92bab3f5466d4c37aba4d
shenqiangbin <shenqiangbin@163.com>
2017-07-26 22:50:51 +0800

    Init

---
+++ PageDesign/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PageDesign.MvcApplication" Language="C#" %>
---
+++ PageDesign/Scripts/tool/button.js
@@ -0,0 +1,11 @@
+﻿var Button = function () {
+    var btn = this;
+    btn.value = "确定";
+    btn.width = 72;
+    btn.height = 26;
+    btn.toHtml = function(){
+        return "<input type='button' style='{0}' value='{1}' />"
+        .replace("{0}", 'width:' + btn.width + 'px;height:' + btn.height + 'px')
+        .replace("{1}", btn.value);
+    };
+};
